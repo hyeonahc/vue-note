d8e94c74ef11d51ed51dc533fb6a22670b353e37
hyeonah <hyeonah.hello@gmail.com>
2022-06-10 15:12:27 +0900

    tutorial: Create vue project with cli

---
+++ heropy/02-vue-project-with-cli/src/App.vue
@@ -0,0 +1,31 @@
+<!-- vue 프로젝트는 template, script, style로 나눠진다 -->
+<!-- 1. template: HTML code -->
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- HelloWorld라는 컴포넌트를 script에서 받아와 출력 -->
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<!-- 2. script: JavaScript code -->
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<!-- 3. style: CSS code -->
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
