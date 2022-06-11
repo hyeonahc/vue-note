a28764a78db90cb31489473e4c1b5397a130667e
hyeonah <hyeonah.hello@gmail.com>
2022-06-11 20:51:52 +0900

    tutorial: Learn vue LifeCycle hooks

---
+++ heropy/04-application-component-instance/src/components/LifeCycle.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 2,
+    };
+  },
+  beforeCreate() {
+    console.log('Before Create');
+  },
+  created() {
+    console.log('Created');
+  },
+  beforeMount() {
+    console.log('Before Mount');
+  },
+  mounted() {
+    console.log('Mounted');
+  },
+};
+</script>
