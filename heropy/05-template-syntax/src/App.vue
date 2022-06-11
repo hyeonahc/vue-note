27a997e1500b157dea41b8692f60144b5c11585a
hyeonah <hyeonah.hello@gmail.com>
2022-06-11 21:35:10 +0900

    tutorial: Learn v-once directive

---
+++ heropy/05-template-syntax/src/App.vue
@@ -0,0 +1,14 @@
+<template><InterpolationComp /></template>
+
+<script>
+import InterpolationComp from './components/InterpolationComp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    InterpolationComp,
+  },
+};
+</script>
+
+<style></style>
---
+++ heropy/05-template-syntax/src/components/InterpolationComp.vue
@@ -0,0 +1,23 @@
+<!-- 1. 보간법 (Interpolation)  -->
+<template>
+  <!-- v-once: 데이터를 초기에 한번만 렌더링해주는 디렉티브 (데이터가 업데이트되어도 변하지 않는다) -->
+  <h1 v-once @click="add">{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'InterpolationComp',
+  data() {
+    return {
+      msg: 'Hello world',
+    };
+  },
+  methods: {
+    add() {
+      this.msg += '!';
+    },
+  },
+};
+</script>
+
+<style scoped></style>
