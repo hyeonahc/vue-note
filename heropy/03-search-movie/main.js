470548c0fdbf6329ed48ef190f426963830b6460
hyeonah <hyeonah.hello@gmail.com>
2022-06-13 18:01:46 +0900

    tutorial: Add intersection observer feature

--- heropy/03-search-movie/main.js
+++ heropy/03-search-movie/main.js
@@ -4,7 +4,9 @@ const App = {
     return {
       title: '',
       movies: [],
+      total: 0,
       page: 1,
+      showObserver: true,
     };
   },
   // Getter 계산된 데이터 (원본 데이터를 계산하는 옵션)
@@ -18,6 +20,22 @@ const App = {
         };
       });
     },
+    hasTheRest() {
+      return this.total > this.movies.length;
+    },
+  },
+  // 라이프사이클 훅 created(): Vue.js 어플리케이션이 생성된 직후에 실행되는 함수
+  mounted() {
+    console.log('created');
+    const io = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          console.log('교차되었음');
+          this.searchMovies();
+        }
+      });
+    });
+    io.observe(this.$refs.observer);
   },
   methods: {
     // 최초 요청 함수
@@ -35,8 +53,22 @@ const App = {
       const { Search, totalResults } = res;
       this.movies.push(...Search);
       this.page += 1;
+      this.total = parseInt(totalResults);
+      console.log(this.total);
+      this.showObserver = false;
+      // setTimeout의 두번째 파라미터로 들어가는 지연시간은 기본값이 0이므로 생략해준다
+      // setTimeout의 시간지연 기능은 자바스크립트가 수행하지 않고 브라우저 즉 Web API가 시간지연 기능을 수행해준다
+      // setTimeout 대신 Vue에서 제공하는 메서드 nextTick을 사용할 수 있다
+      // nextTick: 화면이 갱신되고 난 후에 실행하는 함수
+      // await this.$nextTick();
+      // this.showObserver = true;
+      setTimeout(() => {
+        this.showObserver = true;
+      });
     },
   },
 };
 
 Vue.createApp(App).mount('#app');
+
+// this.$refs.observer
