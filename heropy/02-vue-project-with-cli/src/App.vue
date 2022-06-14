9d6bf25b8f5d65867d1b9175e740cb6c3a25dae6
hyeonah <hyeonah.hello@gmail.com>
2022-06-14 13:56:57 +0900

    tutorial: Update template for future practice

--- heropy/02-vue-project-with-cli/src/App.vue
+++ heropy/02-vue-project-with-cli/src/App.vue
@@ -1,9 +1,5 @@
-<!-- vue 프로젝트는 template, script, style로 나눠진다 -->
-<!-- 1. template: HTML code -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <!-- HelloWorld라는 컴포넌트를 script에서 받아와 출력 -->
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld />
 </template>
 
 <!-- 2. script: JavaScript code -->
@@ -18,14 +14,4 @@ export default {
 };
 </script>
 
-<!-- 3. style: CSS code -->
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
