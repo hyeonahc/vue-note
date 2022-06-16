c3d057aed5932cf31469f98ea1e8a25003484b32
hyeonah <hyeonah.hello@gmail.com>
2022-06-16 12:30:52 +0900

    docs: Update comments

--- heropy/11-form-input-binding/src/components/ModifiersTrim.vue
+++ heropy/11-form-input-binding/src/components/ModifiersTrim.vue
@@ -3,7 +3,7 @@
   <h1>{{ msg }}</h1>
   <!-- input에 입력한 문자 앞뒤에 공백이 있으면 공백을 포함한 데이터가 출력된다  -->
   <!-- <input type="text" v-model="msg" /> -->
-  <!-- trim:  -->
+  <!-- trim: 입력한 데이터 앞뒤에 있는 공백을 제거해준다  -->
   <input type="text" v-model.trim="msg" />
 </template>
 
