af701b87c9ab1ee92641a5281113b2b95395b6d5
hyeonah <hyeonah.hello@gmail.com>
2022-06-16 19:45:42 +0900

    tutorial: Learn how to make components, props and slot

---
+++ heropy/12-components/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <MyBtn>Apple</MyBtn>
+  <!-- props로 color="royalblue"를 전달했다 -->
+  <MyBtn color="royalblue">Apple</MyBtn>
+  <MyBtn color="#000">Apple</MyBtn>
+  <!-- 데이터를 전달해줄때는 v-bind: 또는 :를 key 앞에 써준다 -->
+  <MyBtn :color="color">Apple</MyBtn>
+  <MyBtn large>Apple</MyBtn>
+  <!-- slot태그로 처리할 수 있다 -->
+  <!-- <MyBtn text="Banana" /> -->
+  <MyBtn>Banana</MyBtn>
+</template>
+
+<script>
+import MyBtn from './components/MyBtn.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MyBtn,
+  },
+  data() {
+    return {
+      color: '#000',
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ heropy/12-components/src/components/MyBtn.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :style="{ backgroundColor: color }" :class="{ large }" class="btn">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: 'gray',
+    },
+    large: {
+      type: Boolean,
+      default: false,
+    },
+    // text: {
+    //   type: String,
+    //   default: '',
+    // },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  display: inline-block;
+  margin: 4px;
+  padding: 6px 12px;
+  border-radius: 4px;
+  background-color: gray;
+  color: white;
+  cursor: pointer;
+}
+.btn.large {
+  font-size: 20px;
+  padding: 10px 20px;
+}
+</style>
