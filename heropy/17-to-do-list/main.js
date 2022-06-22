1fc0562ebecd56c4ba82d8ba651a122fb0fe7ff3
hyeonah <hyeonah.hello@gmail.com>
2022-06-22 20:09:46 +0900

    feat: Create todo-item component and add editmode feature

--- heropy/17-to-do-list/main.js
+++ heropy/17-to-do-list/main.js
@@ -3,7 +3,6 @@ import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js';
 const App = {
   data() {
     return {
-      title: '',
       todos: [],
     };
   },
@@ -19,12 +18,56 @@ const App = {
       });
       this.title = '';
     },
-    deleteTodo(todoToDelete) {
-      const index = this.todos.findIndex(todo => todo.id === todoToDelete.id);
-      this.todos.splice(index, 1);
+  },
+};
+
+const TodoItem = {
+  template: /* HTML */ `
+    <li>
+      {{ todo.title }}
+      <template v-if="editMode">
+        <input
+          v-model="newTitle"
+          @keydown.enter="editTodo(todo)"
+          ref="newTitleInput"
+        />
+        <button @click="editTodo(todo)">확인</button>
+      </template>
+      <template v-else>
+        <button @click="onEditMode">수정</button>
+        <button @click="deleteTodo">삭제</button>
+      </template>
+    </li>
+  `,
+  props: {
+    todos: Array,
+    todo: Object,
+  },
+  data() {
+    return {
+      editMode: false,
+      newTitle: '',
+    };
+  },
+  methods: {
+    onEditMode() {
+      this.editMode = true;
+    },
+    offEditMode() {
+      this.editMode = false;
+    },
+    editTodo(todo) {
+      todo.title = this.newTitle;
+      this.offEditMode();
+    },
+    deleteTodo() {},
+    mounted() {
+      console.log(this.$refs);
+      this.$refs.newTitleInput.focus();
     },
   },
 };
 
 const app = Vue.createApp(App);
+app.component('todo-item', TodoItem);
 app.mount('#app');
