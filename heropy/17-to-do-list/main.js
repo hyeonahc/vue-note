387605dc53240c175a93518e936587311b66afcc
hyeonah <hyeonah.hello@gmail.com>
2022-06-23 19:30:32 +0900

    [17-to-do-list] feat: Store data in local storage

--- heropy/17-to-do-list/main.js
+++ heropy/17-to-do-list/main.js
@@ -16,13 +16,27 @@ const App = {
   data() {
     return {
       title: '',
-      todos: [
-        { title: 'ABC', id: nanoid() },
-        { title: 'XYZ', id: nanoid() },
-      ],
+      todos: [],
     };
   },
+  watch: {
+    todos: {
+      handler() {
+        this.saveTodos();
+      },
+      deep: true,
+    },
+  },
+  created() {
+    this.fetchTodos();
+  },
   methods: {
+    fetchTodos() {
+      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+    },
+    saveTodos() {
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    },
     addTodo() {
       if (!this.title.trim()) {
         return;
@@ -34,7 +48,6 @@ const App = {
       this.title = '';
     },
     deleteTodo(todoTodelete) {
-      console.log(todoTodelete);
       const index = this.todos.findIndex(todo => todo.id === todoTodelete.id);
       this.todos.splice(index, 1);
     },
@@ -46,13 +59,10 @@ const TodoItem = {
     <li>
       <template v-if="!editMode">
         <span>{{ todo.title }}</span>
-        <!-- 이벤트 버블링을 막아주는 이벤트 수식어 stop 사용 -->
-        <!-- 이벤트 버블링: 자식 요소를 선택했을때 부모 요소에 있는 이벤트도 모두 실행되는 현상 -->
         <button @click.stop="onEditMode">수정</button>
         <button @click="deleteTodo">삭제</button>
       </template>
       <template v-else>
-        <!-- 이벤트 버블링을 막아주는 이벤트 수식어 stop 사용 -->
         <div @click.stop>
           <input
             ref="inputTitle"
@@ -80,9 +90,7 @@ const TodoItem = {
     async onEditMode() {
       this.editMode = true;
       this.title = this.todo.title;
-      // vue문법을 사용한 이벤트가 아닌 수동으로 추가해준 이벤트 핸들러는 수동으로 삭제해주어야한다 (그렇지 않으면 이벤트가 쌓임)
       window.addEventListener('click', this.offEditMode);
-      // 화면이 바뀐후 할일 지정
       await this.$nextTick();
       this.$refs.inputTitle.focus();
     },
