6dfe9b29ee25f8e077c6e46c61561b105001de98
hyeonah <hyeonah.hello@gmail.com>
2022-07-01 20:59:02 +0900

    [18-vue-project-with-vite] tutorial: Learn v-model, dynamic component with first vite.js project

---
+++ heropy/18-vue-project-with-vite/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- <h1>{{ msg }}</h1>
+  <TextField v-model="msg" /> -->
+  <button @click="compName='ABC'">
+    ABC
+  </button>
+  <button @click="compName='XYZ'">
+    XYZ
+  </button>
+  <!-- 동적 컴포넌트 -->
+  <!-- is="컴포넌트이름"을 넣으면 해당하는 컴포넌트가 리턴된다 -->
+  <Component :is="compName" />
+</template>
+
+<script>
+// import TextField from '~/components/TextField.vue'
+import ABC from '~/components/ABC.vue'
+import XYZ from '~/components/XYZ.vue'
+
+export default {
+  components: {
+    // TextField,
+    ABC,
+    XYZ
+  },
+  data() {
+    return {
+      msg: 'Hello Vue',
+      compName: 'ABC'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ heropy/18-vue-project-with-vite/src/components/TextField.vue
@@ -0,0 +1,25 @@
+<template>
+  <input
+    type="text"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)" />
+</template>
+
+<script>
+export default {
+	props: {
+		modelValue: {
+			type: String,
+			default: ''
+		}
+	},
+	emits: ['update:modelValue']
+}
+</script>
+
+<style scoped>
+/* scoped: 스타일의 유효범위를 정해주는 키워드 */
+input {
+	background-color: rosybrown;
+}
+</style>
