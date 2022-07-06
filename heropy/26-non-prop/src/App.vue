2fd8e8a0b7a8fd32572dd5e2ac8be1881d0adb84
hyeonah <hyeonah.hello@gmail.com>
2022-07-06 17:56:55 +0900

    [26-non-prop] tutorial: Learn non prop

---
+++ heropy/26-non-prop/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- 여기서 class="active"는 prop이 아닌 속성 즉 nonprops 이다-->
+  <!-- nonprop 속성은 컴포넌트의 최상위 요소에게 전달된다 -->
+  <!-- 이때 컴포넌트의 최상위 요소가 하나 이상일 경우에는 전달되지 않는다 -->
+  <!-- 컴포넌트에 등록한 props는 컴포넌트의 최상위 요소에 전달되지 않는다 -->
+  <HelloWorld
+    :message="message"
+    class="active"
+    good="123"
+    @click="message = message.split('').reverse().join('')" />
+</template>
+
+<script>
+import HelloWorld from '~/components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: 'Bye World'
+    }
+  }
+}
+</script>
---
+++ heropy/26-non-prop/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Hello World</h1>
+  <!-- 최상위 요소가 2개 이상일때 non prop 속성을 받아오지 못한다 -->
+  <!-- v-bind="$attrs" 속성을 요소에 붙이면 최상위 요소가 아닌 요소에도 non prop 속성을 받아 올 수 있다 -->
+  <p
+    v-bind="$attrs">
+    {{ message }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: ''
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.active {
+  color: red;
+}
+[good="123"] {
+  text-decoration: underline;
+}
+</style>
