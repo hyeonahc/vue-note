94a7822dd3bd6dce6ca52a2fa53a8d0153e9dd83
hyeonah <hyeonah.hello@gmail.com>
2022-08-08 19:48:54 +0900

    [heropy/16-movie-app] feat: Add error message when there is missing search value

--- heropy/16-movie-app/src/components/MovieList.vue
+++ heropy/16-movie-app/src/components/MovieList.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <div class="inner">
+      {{ message }}
       <MovieItem
         v-for="movie in movies"
         :key="movie.imdbID"
@@ -20,6 +21,9 @@ export default {
 		movies() {
 			// $ 키워드로 store에 접근, state 값 불러오기, movie 모듈 안에 있는 movies state 값을 리턴
 			return this.$store.state.movie.movies
+		},
+		message() {
+			return this.$store.state.movie.message
 		}
 	}
 }
