5d5b7db1b8560e545873d96d70da4e326bff8671
hyeonah <hyeonah.hello@gmail.com>
2022-08-11 17:21:05 +0900

    [heropy/16-movie-app] style: Adjust style with breakpoints

--- heropy/16-movie-app/src/components/Search.vue
+++ heropy/16-movie-app/src/components/Search.vue
@@ -84,6 +84,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~/scss/main";
+
 .container {
 	display: flex;
 	/* container에 모든 자식요소를 선택 */
@@ -109,6 +111,23 @@ export default {
 		height: 50px;
 		font-weight: 700;
 		flex-shrink: 0;
+	}
+	@include media-breakpoint-down(lg) {
+		display: block;
+		input {
+			margin-right: 0;
+			margin-bottom: 10px;
+		}
+		.selects {
+			margin-right: 0;
+			margin-bottom: 10px;
+			select {
+				width: 100%;
+			}
+		}
+		.btn {
+			width: 100%;
+		}
 	}
 }
 </style>
