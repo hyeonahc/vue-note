24092a6b5832d303e7d77b212af2dabcda286ac4
hyeonah <hyeonah.hello@gmail.com>
2022-08-11 16:55:53 +0900

    [heropy/16-movie-app] feat: Create Not Found page

--- heropy/16-movie-app/src/routes/index.js
+++ heropy/16-movie-app/src/routes/index.js
@@ -5,6 +5,7 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from './Home';
 import Movie from './Movie';
 import About from './About';
+import NotFound from './NotFound';
 
 export default createRouter({
   // hash, history
@@ -32,5 +33,9 @@ export default createRouter({
       path: '/about',
       component: About,
     },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound
+    }
   ],
 });
