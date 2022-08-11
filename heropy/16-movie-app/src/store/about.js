3d4853e2bd5b91388e1ef9329793cc0b9a0ef6a7
hyeonah <hyeonah.hello@gmail.com>
2022-08-11 15:27:39 +0900

    [heropy/16-movie-app] feat: Create about store

---
+++ heropy/16-movie-app/src/store/about.js
@@ -0,0 +1,9 @@
+export default {
+	namespaced: true,
+	state: () => ({
+		name: 'HYEONAH',
+		email: 'hyeonah.hello@gmail.com',
+		blog: 'https://maplecoding.tistory.com/',
+		image: 'https://avatars.githubusercontent.com/u/83247825?s=96&v=4'
+	})
+}
