945c79e513bf42453547cd2f55a0c30bff6644c2
hyeonah <hyeonah.hello@gmail.com>
2022-08-14 18:30:47 +0900

    [heropy/28-movie-project-with-router] tutorial: Update tutorial comments about guards

--- heropy/28-movie-project-with-router/src/routes/guards.js
+++ heropy/28-movie-project-with-router/src/routes/guards.js
@@ -12,11 +12,12 @@ router.beforeEach(to => {
 	// About 페이지에 접속할때만 콘솔창에 메타정보가 뜬다
 	// console.log('to.meta.auth', to.meta.auth)
 	if(to.meta.auth) {
-		// Application에 currentUser	{ "name": "Heropy" }를 등록한다
-		// name이 있으면 로그인이 된 상태라고 가정
-		// 통과한 user: about 페이지 접속 가능
-		// 통과하지 못한 user: about 페이지 접속 불가, main 페이지로 돌아감
-		// currentUser에 등록된 사용자 정보가 없다면 about 페이지에 접속하지 못하고 튕겨져 나간다
+		// 로그인한 상태를 만들어주기 위해 웹브라우저 Application에 currentUser의 값으로 { "name": "maple" }를 등록한다
+		// 로그인한 사용자만 들어갈 수 있는 About 컴포넌트에 접속한다
+		// to.meta.auth값으로 true를 가진 About 컴포넌트만 코드가 실행된다 (다른 컴포넌트로 접속하면 guards.js 코드가 실행되지 않음)
+		// currentUser 객체에 있는 값 name을 구조분해할당을 하며 값을 꺼내준다
+		// name의 값이 있으면 (로그인이 된 상태면) true가 리턴되고 About 페이지에 접속이 가능해진다
+		// name의 값이 없으면 (로그인이 안된 상태면) '/'가 리턴되고 Home 페이지로 튕겨져 나간다
 		const { name } = JSON.parse(localStorage.getItem('currentUser') || '{}')
 		if(name) {
 			return true
